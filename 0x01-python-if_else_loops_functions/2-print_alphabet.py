889b78d74b8fd413d88eacd7bfd71c01b2887537
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-06-07 00:37:45 +0100

    alphab

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for char_code in range(ord('a'), ord('z')+1):
-    print(chr(char_code), end='')
-
-print()  # Print a new line after the loop ends
+print(''.join('{}'.format(chr(i)) for i in range(ord('a'), ord('z')+1)), end='')
