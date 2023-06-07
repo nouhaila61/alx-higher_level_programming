34e4264ad0ce90116c3e3d0bd127335afebd9124
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-06-07 01:04:27 +0100

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:d}{:d}, ".format(i, j), end='')
+print(', '.join('{}{}'.format(i, j) for i in range(9) for j in range(i+1, 10)))
