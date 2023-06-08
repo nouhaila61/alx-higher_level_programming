6a189f63da128413c333d76581f2213938ca89b5
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-06-09 00:32:17 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+
+    import sys
+
+    argucnt = len(sys.argv) - 1
+
+    if argucnt == 0:
+        print("0 arguments.")
+    elif argucnt == 1:
+        print('1 argument:')
+    else:
+        print('{} arguments:'.format(argucnt))
+    for i in range(argucnt):
+        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
