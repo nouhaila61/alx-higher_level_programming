7795a85c1fa2ee4d9689d4ce9e8dc8d2b3018dd2
nouhaila61 <nouhailaelbouhali99@gmail.com>
2023-06-09 00:48:40 +0100

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,6 @@ def calculate(a, op, b):
     return dic[op](a, b)
 
 if __name__ == "__main__":
-
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
